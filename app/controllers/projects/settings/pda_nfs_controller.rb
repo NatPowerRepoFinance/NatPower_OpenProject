51df7ad6c6c4267c8a745c45eed40cb88363c491
Hasan <hasanadeym@gmail.com>
2025-12-18 17:40:55 +0500

    Implement seggregated controllers and views

--- app/controllers/projects/settings/pda_nfs_controller.rb
+++ app/controllers/projects/settings/pda_nfs_controller.rb
@@ -39,36 +39,12 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
                      only: %i[
                        index
                        show
+                       show_by_pda_id
                        new
                        create
                        edit
                        update
                        destroy
-                       new_land_contract
-                       create_land_contract
-                       show_land_contract
-                       edit_land_contract
-                       update_land_contract
-                       destroy_land_contract
-                       new_pda_link
-                       create_pda_link
-                       show_pda_link
-                       edit_pda_link
-                       update_pda_link
-                       destroy_pda_link
-                       new_land_parcel
-                       create_land_parcel
-                       show_land_parcel
-                       edit_land_parcel
-                       update_land_parcel
-                       destroy_land_parcel
-                       new_land_title
-                       create_land_title
-                       show_land_title
-                       edit_land_title
-                       update_land_title
-                       destroy_land_title
-                       negotiation_contracts
                      ]
 
   def index
@@ -78,6 +54,7 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
   end
 
   def show
+
     if params[:pda_nf_id].present?
       # Nested route: pda_nfs/:pda_nf_id/negotiations/:id (negotiation show)
       @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
@@ -89,7 +66,13 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
     elsif params[:id].present?
       # Regular route: pda_nfs/:id (PDA show)
       @pda_nf = @project.pda_nfs.find(params[:id])
-      @pda_api_data = fetch_pda_api_data(@pda_nf.pda_id) if @pda_nf.pda_id.present?
+      
+      # Fetch PDA data from API using GisApiService
+      if @pda_nf.pda_id.present?
+        gis_service = ::GisAPI::GisApiService.new
+        result = gis_service.get_pda(@pda_nf.pda_id)
+        @pda_api_data = result.success? ? result.result : nil
+      end
       
       # Get land negotiations from API (don't sync to database)
       @land_negotiations_api_data = []
@@ -104,49 +87,48 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
     end
   end
 
-  def negotiation_contracts
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @negotiation_metadata = {
-      negotiation_id: params[:negotiation_id],
-      code: params[:negotiation_code],
-      name: params[:negotiation_name],
-      friendly_name: params[:negotiation_friendly_name]
-    }
+  # Show PDA by API pda_id (API-only, no database manipulation)
+  def show_by_pda_id
+    pda_id = params[:pda_id]
 
-    unless @negotiation_metadata[:negotiation_id].present?
-      flash[:error] = "Negotiation ID is required to view contracts."
-      return redirect_to project_settings_pda_nf_path(@project, @pda_nf)
+    unless pda_id.present?
+      flash[:error] = "PDA ID is required"
+      redirect_to project_settings_pda_nfs_path(@project)
+      return
     end
 
-    @negotiation_contracts_api_data = fetch_negotiation_contracts_api_data(@negotiation_metadata[:negotiation_id])
-    @negotiation_record_for_api_contract =
-      @pda_nf.land_negotiation_nfs.find_by(land_negotiation_id: @negotiation_metadata[:negotiation_id]) ||
-      (@negotiation_metadata[:code].present? && @pda_nf.land_negotiation_nfs.find_by(code: @negotiation_metadata[:code]))
+    # Fetch PDA data from API using GisApiService
+    gis_service = ::GisAPI::GisApiService.new
+    result = gis_service.get_pda(pda_id)
 
-    if @negotiation_contracts_api_data.nil?
-      @negotiation_contracts_api_data = []
-      flash.now[:error] = "Unable to load contracts from the external API right now."
+    unless result.respond_to?(:success?) && result.success?
+      flash[:error] = "PDA with ID #{pda_id} not found in the API"
+      redirect_to project_settings_pda_nfs_path(@project)
+      return
     end
 
-    # Fetch land titles from API for this negotiation
-    @land_titles_api_data = []
-    @pda_api_data = fetch_pda_api_data(@pda_nf.pda_id) if @pda_nf.pda_id.present?
-    
-    if @pda_api_data.present? && @pda_api_data["negotiationRel"].present?
-      negotiation_api_data = @pda_api_data["negotiationRel"].find do |neg|
-        neg_id = neg["landNegotiationId"] || neg["id"]
-        # Match by land_negotiation_id or code
-        neg_id == @negotiation_metadata[:negotiation_id].to_s || neg["code"] == @negotiation_metadata[:code]
-      end
-      
-      if negotiation_api_data.present?
-        # Get land titles from API for this negotiation (raw API data - not stored in DB)
-        if negotiation_api_data["titleNo"].present? && negotiation_api_data["titleNo"].is_a?(Array)
-          @land_titles_api_data = negotiation_api_data["titleNo"]
-        end
-      end
+    @pda_api_data = result.result
+
+    unless @pda_api_data.present?
+      flash[:error] = "PDA with ID #{pda_id} not found in the API"
+      redirect_to project_settings_pda_nfs_path(@project)
+      return
     end
+
+    # Extract land negotiations from API data
+    @land_negotiations_api_data = []
+    if @pda_api_data["negotiationRel"].present? && @pda_api_data["negotiationRel"].is_a?(Array)
+      @land_negotiations_api_data = @pda_api_data["negotiationRel"]
+    end
+
+    # No database records - API only
+    @land_parcels = []
+    @pda_id = pda_id
+
+    # Render the same view as regular show action
+    render :show
   end
+
 
   def new
     if params[:pda_nf_id].present?
@@ -256,248 +238,6 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
     end
   end
 
-  def new_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.build(land_negotiation_id: @land_negotiation.id)
-  end
-
-  def create_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.build(land_contract_params)
-    @land_contract.land_negotiation_id = @land_negotiation.id
-
-    if @land_contract.save
-      flash[:notice] = I18n.t(:notice_successful_create)
-      redirect_to project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_create_with_reason, reason: @land_contract.errors.full_messages.join(", "))
-      render action: :new_land_contract, status: :unprocessable_entity
-    end
-  end
-
-  def show_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.find(params[:land_contract_id])
-  end
-
-  def edit_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.find(params[:land_contract_id])
-  end
-
-  def update_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.find(params[:land_contract_id])
-
-    if @land_contract.update(land_contract_params)
-      flash[:notice] = I18n.t(:notice_successful_update)
-      redirect_to show_land_contract_project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation, @land_contract)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_update_with_reason, reason: @land_contract.errors.full_messages.join(", "))
-      render action: :edit_land_contract, status: :unprocessable_entity
-    end
-  end
-
-  def destroy_land_contract
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @land_contract = @land_negotiation.land_contracts_nfs.find(params[:land_contract_id])
-
-    if @land_contract.destroy
-      flash[:notice] = I18n.t(:notice_successful_delete)
-    else
-      flash[:error] = I18n.t(:notice_unsuccessful_delete)
-    end
-
-    redirect_to project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation)
-  end
-
-  def new_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.build(land_negotiation_id: @land_negotiation.id)
-    @available_pdas = @project.pda_nfs.where.not(id: @land_negotiation.linked_pdas.pluck(:id))
-  end
-
-  def create_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.build(pda_link_params)
-    @pda_link.land_negotiation_id = @land_negotiation.id
-
-    if @pda_link.save
-      flash[:notice] = I18n.t(:notice_successful_create)
-      redirect_to project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_create_with_reason, reason: @pda_link.errors.full_messages.join(", "))
-      @available_pdas = @project.pda_nfs.where.not(id: @land_negotiation.linked_pdas.pluck(:id))
-      render action: :new_pda_link, status: :unprocessable_entity
-    end
-  end
-
-  def show_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.find(params[:pda_link_id])
-  end
-
-  def edit_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.find(params[:pda_link_id])
-    @available_pdas = @project.pda_nfs.where.not(id: @land_negotiation.linked_pdas.where.not(id: @pda_link.pda_id).pluck(:id))
-  end
-
-  def update_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.find(params[:pda_link_id])
-
-    if @pda_link.update(pda_link_params)
-      flash[:notice] = I18n.t(:notice_successful_update)
-      redirect_to show_pda_link_project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation, @pda_link)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_update_with_reason, reason: @pda_link.errors.full_messages.join(", "))
-      @available_pdas = @project.pda_nfs.where.not(id: @land_negotiation.linked_pdas.where.not(id: @pda_link.pda_id).pluck(:id))
-      render action: :edit_pda_link, status: :unprocessable_entity
-    end
-  end
-
-  def destroy_pda_link
-    @pda_nf = @project.pda_nfs.find(params[:pda_nf_id])
-    @land_negotiation = @pda_nf.land_negotiation_nfs.find(params[:id])
-    @pda_link = @land_negotiation.land_negotiation_pda_link_nfs.find(params[:pda_link_id])
-
-    if @pda_link.destroy
-      flash[:notice] = I18n.t(:notice_successful_delete)
-    else
-      flash[:error] = I18n.t(:notice_unsuccessful_delete)
-    end
-
-    redirect_to project_settings_pda_nf_negotiation_path(@project, @pda_nf, @land_negotiation)
-  end
-
-  def new_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.build(pda_id: @pda_nf.id)
-  end
-
-  def create_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.build(land_parcel_params)
-    @land_parcel.pda_id = @pda_nf.id
-
-    if @land_parcel.save
-      flash[:notice] = I18n.t(:notice_successful_create)
-      redirect_to project_settings_pda_nf_path(@project, @pda_nf)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_create_with_reason, reason: @land_parcel.errors.full_messages.join(", "))
-      render action: :new_land_parcel, status: :unprocessable_entity
-    end
-  end
-
-  def show_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_titles = @land_parcel.land_titles_nfs.order(:land_title_id)
-  end
-
-  def edit_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-  end
-
-  def update_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-
-    if @land_parcel.update(land_parcel_params)
-      flash[:notice] = I18n.t(:notice_successful_update)
-      redirect_to show_land_parcel_project_settings_pda_nf_path(@project, @pda_nf, land_parcel_id: @land_parcel.id)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_update_with_reason, reason: @land_parcel.errors.full_messages.join(", "))
-      render action: :edit_land_parcel, status: :unprocessable_entity
-    end
-  end
-
-  def destroy_land_parcel
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-
-    if @land_parcel.destroy
-      flash[:notice] = I18n.t(:notice_successful_delete)
-    else
-      flash[:error] = I18n.t(:notice_unsuccessful_delete)
-    end
-
-    redirect_to project_settings_pda_nf_path(@project, @pda_nf)
-  end
-
-  def new_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.build(land_parcel_id: @land_parcel.id)
-  end
-
-  def create_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.build(land_title_params)
-    @land_title.land_parcel_id = @land_parcel.id
-
-    if @land_title.save
-      flash[:notice] = I18n.t(:notice_successful_create)
-      redirect_to show_land_parcel_project_settings_pda_nf_path(@project, @pda_nf, land_parcel_id: @land_parcel.id)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_create_with_reason, reason: @land_title.errors.full_messages.join(", "))
-      render action: :new_land_title, status: :unprocessable_entity
-    end
-  end
-
-  def show_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.find_by!(land_title_id: params[:land_title_id])
-  end
-
-  def edit_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.find_by!(land_title_id: params[:land_title_id])
-  end
-
-  def update_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.find_by!(land_title_id: params[:land_title_id])
-
-    if @land_title.update(land_title_params)
-      flash[:notice] = I18n.t(:notice_successful_update)
-      redirect_to show_land_title_project_settings_pda_nf_path(@project, @pda_nf, land_parcel_id: @land_parcel.id, land_title_id: @land_title.land_title_id)
-    else
-      flash.now[:error] = I18n.t(:notice_unsuccessful_update_with_reason, reason: @land_title.errors.full_messages.join(", "))
-      render action: :edit_land_title, status: :unprocessable_entity
-    end
-  end
-
-  def destroy_land_title
-    @pda_nf = @project.pda_nfs.find(params[:id])
-    @land_parcel = @pda_nf.land_parcels.find(params[:land_parcel_id])
-    @land_title = @land_parcel.land_titles_nfs.find_by!(land_title_id: params[:land_title_id])
-
-    if @land_title.destroy
-      flash[:notice] = I18n.t(:notice_successful_delete)
-    else
-      flash[:error] = I18n.t(:notice_unsuccessful_delete)
-    end
-
-    redirect_to show_land_parcel_project_settings_pda_nf_path(@project, @pda_nf, land_parcel_id: @land_parcel.id)
-  end
 
   private
 
@@ -534,42 +274,6 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
     end
   end
 
-  def fetch_negotiation_contracts_api_data(negotiation_id)
-    return nil unless negotiation_id.present?
-    return nil unless api_key.present?
-
-    begin
-      url = "https://natpower-gis-project-dev.azurewebsites.net/erp/contract/#{CGI.escape(negotiation_id.to_s)}"
-      Rails.logger.info("PDA API: Fetching negotiation contracts from #{url}")
-
-      response = OpenProject.httpx.with(
-        headers: {
-          "X-Access-Token" => api_key,
-          "Content-Type" => "application/json"
-        }
-      ).get(url)
-
-      Rails.logger.info("PDA API: Negotiation contracts response status - #{response.status}")
-
-      return nil unless response.status == 200
-
-      data = response.json(symbolize_keys: false)
-      payload = data.is_a?(Hash) ? (data["data"] || data["contracts"] || data.values_at("contract").compact.first) : data
-
-      case payload
-      when Array
-        payload
-      when Hash
-        [payload]
-      else
-        []
-      end
-    rescue StandardError => e
-      Rails.logger.error("Failed to fetch negotiation contracts data: #{e.message}")
-      Rails.logger.error("Negotiation contracts backtrace: #{e.backtrace.first(5).join("\n")}")
-      nil
-    end
-  end
 
   def api_key
     ENV["GIS_API_KEY"]
@@ -694,44 +398,6 @@ class Projects::Settings::PdaNfsController < Projects::SettingsController
     )
   end
 
-  def land_contract_params
-    params.require(:land_contract_nf).permit(
-      :contract_id,
-      :contract_type_id,
-      :start_date,
-      :completion_date,
-      :status,
-      :initial_expiry_date,
-      :initial_expiry_notice_period_days,
-      :extension_period,
-      :long_stop_date,
-      :contract_document_link,
-      :contract_description
-    )
-  end
-
-  def pda_link_params
-    params.require(:land_negotiation_pda_link_nf).permit(
-      :pda_id
-    )
-  end
-
-  def land_parcel_params
-    params.require(:land_parcel_nf).permit(
-      :land_parcel_id,
-      :land_parcel_code
-    )
-  end
-
-  def land_title_params
-    params.require(:land_title_nf).permit(
-      :land_title_id,
-      :land_registry_title_id,
-      :land_registry_id,
-      :land_registry_title_document_link,
-      :land_registry_title_request_date
-    )
-  end
 end
 
 
--- modules/grids/app/components/grids/widgets/pdas.rb
+++ modules/grids/app/components/grids/widgets/pdas.rb
@@ -9,6 +9,7 @@ module Grids
 
       def initialize(...)
         super
+        @pdas = nil
       end
 
       def title
@@ -19,8 +20,94 @@ module Grids
         project.present?
       end
 
-      def pda_nfs
-        @pda_nfs ||= project.pda_nfs.order(created_date: :desc).to_a
+      # Fetch PDAs from the API cluster endpoint
+      # Returns array of PDAs with name and id
+      # Always returns an array, never nil
+      def pdas
+        return @pdas if defined?(@pdas) && @pdas.is_a?(Array)
+
+        @pdas = fetch_pdas_from_api
+        @pdas = [] unless @pdas.is_a?(Array)
+        @pdas
+      end
+
+      # Get the PDA database record for a given API PDA id
+      # Returns the PdaNf record if found, nil otherwise
+      def pda_record(pda_id)
+        return nil unless project.present? && pda_id.present?
+        return nil unless project.respond_to?(:pda_nfs)
+
+        @pda_records ||= {}
+        @pda_records[pda_id] ||= project.pda_nfs.find_by(pda_id: pda_id.to_i)
+      end
+
+      # Get the URL for a PDA show page
+      # Uses by_pda_id route which works even if PDA doesn't exist in database
+      def pda_path(pda_id)
+        return nil unless project.present? && pda_id.present?
+
+        by_pda_id_project_settings_pda_nfs_path(project, pda_id: pda_id)
+      end
+
+      private
+
+      def fetch_pdas_from_api
+        return [] unless project.present?
+
+        # Get project ID - works for both database projects and API adapter projects
+        project_id = project.respond_to?(:id) ? project.id : nil
+        return [] unless project_id.present?
+
+        begin
+          gis_service = ::GisAPI::GisApiService.new
+          result = gis_service.get_cluster(project_id)
+
+
+          unless result.respond_to?(:success?) && result.success?
+            Rails.logger.warn("PDAs Widget: Failed to fetch cluster data for project #{project_id}")
+            return []
+          end
+
+          cluster_data = result.result
+          
+          return [] unless cluster_data.is_a?(Hash)
+
+          
+
+          # Extract features array from cluster data
+          # Features are PDAs in the cluster response
+          features = cluster_data.dig("data").dig("cluster").dig("features") || []
+          
+          return [] unless features.is_a?(Array)
+
+          # Map features to PDAs with name and id from properties
+          pdas_array = features.filter_map do |feature|
+            next unless feature.is_a?(Hash)
+
+            properties = feature["properties"] || feature[:properties] || {}
+            next if properties.empty?
+
+            pda_id = properties["id"] || properties[:id]
+            pda_name = properties["name"] || properties[:name]
+            status = properties["status"] || properties[:status]
+
+            # Only include PDAs that have at least an id or name
+            if pda_id.present? || pda_name.present?
+              {
+                id: pda_id,
+                name: pda_name,
+                status: status
+              }
+            end
+          end
+
+          # Ensure we return an array
+          Array(pdas_array)
+        rescue StandardError => e
+          Rails.logger.error("PDAs Widget: Error fetching PDAs from API: #{e.message}")
+          Rails.logger.error("PDAs Widget backtrace: #{e.backtrace.first(10).join("\n")}")
+          []
+        end
       end
     end
   end
